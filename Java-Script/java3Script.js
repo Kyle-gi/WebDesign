8fda4667c0cafce65bf377bf82b0ac86e24e8d4d
Your Name <Kyle-gi@users.noreply.github.com.>
2024-10-20 22:39:19 +0200

    first commit

---
+++ Java-Script/java3Script.js
@@ -0,0 +1,3 @@
+const array = [1, 1, 2, 2, 3, 3, 4, 4];
+const set = new Set(array);
+console.log("Here is the set value of array.", set);
---
+++ Java-Script/java4Script.js
@@ -0,0 +1,16 @@
+//Define a function called `add` that takes two arguments, `a` and `b`, and returns their sum.
+function add(a, b) {
+    //Return the sum of `a` and `b`.
+    return a + b;
+}
+
+//Define some variables.
+const num1 = 5;
+const num2 = 10;
+
+//Use the `add` function to calculate the sum of `num1` and `num2`.
+
+const sum = add(num1,num2);
+
+//print the results 
+console.log(`The sum of ${num1} and ${num2} is ${sum}.`);
