5230f4092645c3d549cfb88891e24eb3f3196c38
Your Name <Kyle-gi@users.noreply.github.com.>
2024-10-21 22:22:37 +0200

    first commit

---
+++ Java-Script/java6Script.js
@@ -0,0 +1,5 @@
+let arr = [1, 2, 3];
+let obj = {name: "John", age: 30};
+
+console.log(arr instanceof Array);
+console.log(obj instanceof Object);
---
+++ Java-Script/java7Script.js
@@ -0,0 +1,5 @@
+let arr = [1, 2, 3];
+let obj = {name: "John", age: 30};
+
+console.log(Array.isArray(arr));
+console.log(Array.isArray(obj));
