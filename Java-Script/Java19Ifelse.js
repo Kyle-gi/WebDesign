4c3f72d0c99ab57e1a01510bf8a736acaffacce7
Your Name <Kyle-gi@users.noreply.github.com.>
2024-12-23 12:41:51 +0100

    changes

---
+++ Java-Script/Java19Ifelse.js
@@ -0,0 +1,14 @@
+const expression = 'Apple';
+switch (expression){
+    case 'Oranges':
+        console.log('I prefer Oranges');
+        break;
+    case 'Watermelons':
+        console.log('I prefer watermelon!');
+        break;
+    case 'Apple':
+        console.log('One apple a day keeps the doctir away');
+        break;
+        default:
+            console.log(`Sorry, we are out of ${expression}`);
+}
